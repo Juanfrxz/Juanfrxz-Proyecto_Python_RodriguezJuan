4b263ab972b53dc95ded3804dd3b7316770006d8
Juanfrxz <90869650+Juanfrxz@users.noreply.github.com>
2024-10-04 07:57:43 -0500

    Menu principal

---
+++ Modules/utils/ui.py
@@ -0,0 +1,11 @@
+menu="""
+1. Jugar 1v1 
+2. Encontra de la maquina
+3. Estadisticas
+4. Salir
+"""
+selectPiedra = """
+1. Piedra
+2. Papel
+3. Tijera
+"""
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-
-
+import Modules.utils.mensajes as msg
+import Modules.utils.ui as iu
 
 
 
 
 if __name__ == '__main__':
+    isActive = True
+    opMenu = 0
+    while (isActive):
+        try:
+            print(msg.tituloPrincipal)
+            print(iu.menu)
+            opMenu = int(input('づ￣ 3￣)づ⮞ '))
+            match opMenu:
+                case 1:
+                    opMenuSelect = 0
+                    
+                    
+        except ValueError:
+            print('La opcion ingresada no es valida')
+            continue
     pass
